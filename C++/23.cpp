e4b1e95dfb763b6720257803d433619f1c76e0f8
reaper <saip589@gmail.com>
2023-10-26 04:09:28 +0530

    add 23.cpp

---
+++ C++/23.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "info.h"
+
+int main() {
+  int number;
+  std::cout << "Enter a number: ";
+  std::cin >> number;
+
+  if (number > 0) {
+      goto positive;
+  } else if (number < 0) {
+      goto negative;
+  } else {
+      goto zero;
+  }
+
+  positive:
+    std::cout << "The number is positive." << std::endl;
+    goto end;
+
+  negative:
+    std::cout << "The number is negative." << std::endl;
+    goto end;
+
+  zero:
+    std::cout << "The number is zero." << std::endl;
+
+  end:
+    return 0;
+}
