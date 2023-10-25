1523744337371fa883ac3ce6d3ef2f3e723dc1f6
reaper <saip589@gmail.com>
2023-10-25 22:20:22 +0530

    add prog 10, 11 and 12

---
+++ 10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "info.h"
+
+int main() {
+  info();
+  int a,b,c;
+  std::cout << "enter three numbers a, b and c:" << std::endl;
+  std::cin >> a >> b >> c;
+  std::cout << "largest of the three is: " << (((a>b)&&(a>c))?a:((b>a)&&(b>c))?b:c) << std::endl;
+  return 0;
+}
---
+++ 12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "info.h"
+
+int main() {
+  info();
+  std::cout << "this program prints numbers from 2 to 20 using for loop:" << std::endl;
+  for (int i = 1; i <= 10; i++) {
+    std::cout << i*2 << std::endl;
+  }
+  return 0;
+}
