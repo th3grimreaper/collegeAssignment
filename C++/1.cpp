9b8be93a88a1f256f36fc40f7aa5b9edc33e24a9
reaper <saip589@gmail.com>
2023-10-26 03:56:01 +0530

    update 1.cpp

--- C++/1.cpp
+++ C++/1.cpp
@@ -4,6 +4,7 @@
 int main() {
   info();
   std::cout << "size of char is " << sizeof(char) << std::endl;
+  std::cout << "size of bool is " << sizeof(bool) << std::endl;
   std::cout << "size of int is " << sizeof(int) << std::endl;
   std::cout << "size of float is " << sizeof(float) << std::endl;
   std::cout << "size of double is " << sizeof(double) << std::endl;
