65cf8b31e1706a74c7f5ea497f9f5eaa13e4ef06
reaper <saip589@gmail.com>
2023-12-15 14:54:10 +0530

    add programs 65 and 66

--- C++/65.cpp
+++ C++/65.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <ostream>
 
 #include "info.h"
 
+class Complex {
+  int real;
+  int img;
+
+public:
+  Complex(int r, int i) {
+    real = r;
+    img = i;
+  }
+
+  void display() { std::cout << real << " + " << img << "i" << std::endl; }
+  friend Complex operator+(Complex, Complex);
+};
+
+Complex operator+(Complex a, Complex b) {
+  Complex temp(0, 0);
+  temp.real = a.real + b.real;
+  temp.img = a.img + b.img;
+  return temp;
+}
+
 int main() {
   info();
+  Complex obj1(1, 2), obj2(3, 4), obj3(0, 0);
+  obj1.display();
+  obj2.display();
+  std::cout << "Adding obj1 and obj2, we get: ";
+  obj3 = obj1 + obj2;
+  obj3.display();
   return 0;
 }
---
+++ C++/66.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <ostream>
+
+#include "info.h"
+
+class Complex {
+  int x;
+
+public:
+  Complex(int x) { this->x = x; }
+
+  void display() { std::cout << "value of x is: " << x << std::endl; }
+  friend Complex operator-(Complex);
+};
+
+Complex operator-(Complex a) {
+  a.x = -a.x;
+  return a;
+}
+
+int main() {
+  info();
+  Complex obj1(34);
+  obj1.display();
+  std::cout << "After applying unary minus operator on x, we get: ";
+  obj1 = -obj1;
+  obj1.display();
+  return 0;
+}
